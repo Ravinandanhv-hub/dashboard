a50015b27c89a1aa53f3ea5eb896da633fef4d4f
ravinandanhv <ravinandan@appveen.com>
2024-01-06 16:27:00 +0530

    v16 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+  constructor(private fb: FormBuilder){
+
+  }
 }
